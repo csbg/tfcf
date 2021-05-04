f0be2d05134796084f39863e888e213f8870ed9a
nfortelny <nfortelny@sbg.ac.at>
2021-05-04 13:28:04 +0200

    x

--- src/INT_00_pipeline.sh
+++ src/INT_00_pipeline.sh
@@ -1,10 +1,11 @@
 
 
-cd $HOME/omicstmp/
+cd $HOME/omicstmp/nf/
 
-echo "sample_id\tmolecule_h5" > AGGR1.aggr.csv
-echo "ECCITE1\t$DATA/ECCITE1_RNA_cellranger/outs/molecule_info.h5"  >> AGGR1.aggr.csv
-echo "CITESEQ1\t$DATA/CITESEQ1_CLEAN/outs/molecule_info.h5" >> AGGR1.aggr.csv
+echo "sample_id,molecule_h5" > AGGR1.aggr.csv
+echo "ECCITE1,$DATA/ECCITE1_RNA_cellranger/outs/molecule_info.h5"  >> AGGR1.aggr.csv
+echo "CITESEQ1,$DATA/CITESEQ1/outs/molecule_info.h5" >> AGGR1.aggr.csv
 
-cd $HOME/omicstmp/
-~/code/cellranger-6.0.1/cellranger aggr --id=AGGR1 --csv=AGGR1.aggr.csv --normalize=none
+cat AGGR1.aggr.csv
+
+$HOME/code/cellranger-6.0.1/cellranger aggr --id=AGGR1 --csv=AGGR1.aggr.csv --normalize=none &> AGGR1.log
