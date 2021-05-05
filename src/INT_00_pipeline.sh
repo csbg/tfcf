8264f90bf88b9eaeb2813544a76a5edb3846d159
Nikolaus Fortelny <nfortelny@came.sbg.ac.at>
2021-05-05 13:37:55 +0200

    x

--- src/INT_00_pipeline.sh
+++ src/INT_00_pipeline.sh
@@ -1,10 +1,9 @@
 
 
-cd $HOME/omicstmp/
+cd $HOME/omicstmp/nf/
 
 echo "sample_id\tmolecule_h5" > AGGR1.aggr.csv
-echo "ECCITE1\t$DATA/ECCITE1_RNA_cellranger/outs/molecule_info.h5"  >> AGGR1.aggr.csv
+echo "ECCITE1\t$DATA/ECCITE1_RNA_cellranger_601/outs/molecule_info.h5"  >> AGGR1.aggr.csv
 echo "CITESEQ1\t$DATA/CITESEQ1_CLEAN/outs/molecule_info.h5" >> AGGR1.aggr.csv
 
-cd $HOME/omicstmp/
 ~/code/cellranger-6.0.1/cellranger aggr --id=AGGR1 --csv=AGGR1.aggr.csv --normalize=none
