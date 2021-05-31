ad46a56fee31b6db4ea7b585e85beeb085c6e236
nfortelny <nfortelny@sbg.ac.at>
2021-05-31 11:12:50 +0200

    x

--- src/ECCITE2_00_pipeline.sh
+++ src/ECCITE2_00_pipeline.sh
@@ -1,6 +1,4 @@
-basedir=$HOME/GFS/PROJECTS/TfCf/
-
-cd Data/Raw_data_ECCITE1
+cd $GFS/PROJECTS/TfCf/Data/Raw_ECCITE2
 
 python $CODEBASE/tfcf/crukci_to_illumina.py
 
@@ -10,13 +8,13 @@ mkdir -p $HOME/omicstmp/nf
 cd $HOME/omicstmp/nf
 
 ~/code/cellranger-6.0.1/cellranger count --id=ECCITE2 \
-	--no-bam \
-    --libraries=$CODEBASE/tfcf/metadata/ECCITE1_Library.csv \
-    --transcriptome=$HOME/GFS/RESOURCES/Genomes/refdata-gex-mm10-2020-A/ \
-    --feature-ref=$CODEBASE/tfcf/metadata/ECCITE1_Features.csv \
-    --localcores=24 \
-    --localmem=64 \
-    --expect-cells=10000 &> ECCITE1.log
+ --no-bam \
+ --libraries=$CODEBASE/tfcf/metadata/ECCITE2_Library.csv \
+ --transcriptome=$HOME/GFS/RESOURCES/Genomes/refdata-gex-mm10-2020-A/ \
+ --feature-ref=$CODEBASE/tfcf/metadata/ECCITE2_Features.csv \
+ --localcores=24 \
+ --localmem=64 \
+ --expect-cells=10000 &> ECCITE2.log
 
-mkdir -p ~/GFS/PROJECTS/TfCf/Data/ECCITE1/
-cp -R ECCITE1/outs ~/GFS/PROJECTS/TfCf/Data/ECCITE1/
+mkdir -p ~/GFS/PROJECTS/TfCf/Data/ECCITE2/
+cp -R ECCITE2/outs ~/GFS/PROJECTS/TfCf/Data/ECCITE2/
