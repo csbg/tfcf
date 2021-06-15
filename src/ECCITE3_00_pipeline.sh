d5d674fab8e9f2a4c6e7db4655a3f4e54cc8a0bf
Nikolaus Fortelny <nfortelny@came.sbg.ac.at>
2021-06-15 16:16:44 +0200

    x

--- src/ECCITE3_00_pipeline.sh
+++ src/ECCITE3_00_pipeline.sh
@@ -39,10 +39,53 @@ source $CODEBASE/tfcf/setup.sh
 # done
 
 
+
+### CREATE GENOME
+
 cd $HOME/omicstmp
 
-#for id in ECCITE3_low_7d ECCITE3_low_14d ECCITE3_high_7d ECCITE3_high_14d; do
-for id in ECCITE3_low_7d; do
+mkdir newGenome/
+oldGenomePath="$GFS/RESOURCES/Genomes/refdata-gex-mm10-2020-A/"
+
+cp $oldGenome/fasta/genome.fa newGenome/genome.fa
+cp $oldGenome/genes/genes.gtf newGenome/genes.gtf
+
+x="GFP"
+for x in GFP BFP; do
+	pathFA=$CODEBASE/tfcf/metadata/${x}.fa
+
+	numberBases=$(cat $pathFA | grep -v "^>" | tr -d "\n" | wc -c)
+	gtf="$x\tunknown\texon\t1\t${numberBases}\t.\t+\t.\tgene_id xxx${x}xxx; transcript_id xxx${x}xxx; gene_name xxx${x}xxx; gene_biotype xxxprotein_codingxxx;"
+	gtf=$(echo $gtf | sed 's/xxx/"/g')
+
+	echo -e $gtf > ${x}.gtf
+
+	cat ${x}.gtf >> newGenome/genes.gtf
+	fold -w 60 $pathFA >> newGenome/genome.fa
+	
+	echo -e "" >> newGenome/genome.fa
+
+	rm ${x}.gtf
+done
+
+
+grep ">" newGenome/genome.fa
+tail -5 newGenome/genes.gtf
+tail -200 newGenome/genome.fa
+
+
+~/code/cellranger-6.0.1/cellranger mkref --genome=newGenomeExtended --fasta=newGenome/genome.fa --genes=newGenome/genes.gtf
+
+
+
+
+######### BASIC ANALYSIS
+
+
+cd $HOME/omicstmp
+
+for id in ECCITE3_low_7d ECCITE3_low_14d ECCITE3_high_7d ECCITE3_high_14d; do
+#for id in ECCITE3_low_7d; do
         
     echo $id
     #id="ECCITE3_low_7d"
@@ -54,16 +97,51 @@ for id in ECCITE3_low_7d; do
     echo "Features"
     cat $CODEBASE/tfcf/metadata/ECCITE3_Features.csv
     
-~/code/cellranger-6.0.1/cellranger count --id=$id \
- --no-bam \
- --libraries=$CODEBASE/tfcf/metadata/${id}_Library.csv \
- --transcriptome=$HOME/GFS/RESOURCES/Genomes/refdata-gex-mm10-2020-A/ \
- --feature-ref=$CODEBASE/tfcf/metadata/ECCITE3_Features.csv \
- --localcores=24 \
- --localmem=128 \
- --expect-cells=10000 &> ${id}.log
+	~/code/cellranger-6.0.1/cellranger count --id=$id \
+	 --no-bam \
+	 --libraries=$CODEBASE/tfcf/metadata/${id}_Library.csv \
+	 --transcriptome=newGenomeExtended/ \
+	 --feature-ref=$CODEBASE/tfcf/metadata/ECCITE3_Features.csv \
+	 --localcores=24 \
+	 --localmem=128 \
+	 --expect-cells=10000 &> ${id}.log
     
     rm -rf $id/SC_RNA_COUNTER_CS
     
     mv ${id}* $GFS/PROJECTS/TfCf/Data/
+done
+
+
+
+
+
+######### LOOK FOR GUIDES IN EXPRESSION
+
+
+cd $HOME/omicstmp
+
+for id in ECCITE3_low_7d ECCITE3_low_14d ECCITE3_high_7d ECCITE3_high_14d; do
+        
+    echo $id
+    #id="ECCITE3_low_7d"
+    #id="ECCITE3_low_14d"
+    
+    echo "Files"
+    cat $CODEBASE/tfcf/metadata/${id}_Library_CRISPR.csv
+    
+    echo "Features"
+    cat $CODEBASE/tfcf/metadata/ECCITE3_Features.csv
+    
+	~/code/cellranger-6.0.1/cellranger count --id=${id}_CRISPR \
+	 --no-bam \
+	 --libraries=$CODEBASE/tfcf/metadata/${id}_Library_CRISPR.csv \
+	 --transcriptome=newGenomeExtended/ \
+	 --feature-ref=$CODEBASE/tfcf/metadata/ECCITE3_Features.csv \
+	 --localcores=24 \
+	 --localmem=128 \
+	 --expect-cells=10000 &> ${id}_CRISPR.log
+    
+    rm -rf ${id}_CRISPR/SC_RNA_COUNTER_CS
+    
+    mv ${id}_CRISPR* $GFS/PROJECTS/TfCf/Data/
 done
