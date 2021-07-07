d613f549528ddfa6defa46bd2b5c23b07ec2be60
Nikolaus Fortelny <nfortelny@came.sbg.ac.at>
2021-07-07 10:26:21 +0200

    x

---
+++ src/CITESEQ2_00_InitialPipeline.sh
@@ -0,0 +1,61 @@
+source ~/code/tfcf/setup.sh
+
+cd $GFS/PROJECTS/TfCf/NewData/
+python ~/code/tfcf/crukci_to_illumina.py
+
+
+### CREATE GENOME
+
+cd $HOME/omicstmp
+
+mkdir newGenome/
+oldGenome="$GFS/RESOURCES/Genomes/refdata-gex-mm10-2020-A/"
+echo "ORIGINAL GENOME: $oldGenome"
+
+cp $oldGenome/fasta/genome.fa newGenome/genome.fa
+cp $oldGenome/genes/genes.gtf newGenome/genes.gtf
+
+x="GFP"
+for x in GFP BFP; do
+	pathFA=$CODEBASE/tfcf/metadata/${x}.fa
+
+	numberBases=$(cat $pathFA | grep -v "^>" | tr -d "\n" | wc -c)
+	gtf="$x\tunknown\texon\t1\t${numberBases}\t.\t+\t.\tgene_id xxx${x}xxx; transcript_id xxx${x}xxx; gene_name xxx${x}xxx; gene_biotype xxxprotein_codingxxx;"
+	gtf=$(echo $gtf | sed 's/xxx/"/g')
+
+	echo -e $gtf > ${x}.gtf
+    l 
+	cat ${x}.gtf >> newGenome/genes.gtf
+	fold -w 60 $pathFA >> newGenome/genome.fa
+	
+	echo -e "" >> newGenome/genome.fa
+
+	rm ${x}.gtf
+done
+
+
+grep ">" newGenome/ls -algenome.fa
+tail -5 newGenome/genes.gtf
+tail -200 newGenome/genome.fa
+
+
+~/code/cellranger-6.0.1/cellranger mkref --genome=newGenomeExtended --fasta=newGenome/genome.fa --genes=newGenome/genes.gtf
+
+
+
+
+# ANALYZE WITH ANTIBODIES
+cd $HOME/omicstmp/
+
+~/code/cellranger-6.0.1/cellranger count --id=CITESEQ2 \
+	--no-bam \
+    --libraries=$CODEBASE/tfcf/metadata/CITESEQ2_Library.csv \
+    --transcriptome=newGenomeExtended/ \
+    --feature-ref=$CODEBASE/tfcf/metadata/CITESEQ2_Features.csv \
+    --localcores=24 \
+    --localmem=64 \
+    --expect-cells=10000 &> CITESEQ2.log
+
+mkdir -p ~/GFS/PROJECTS/TfCf/Data/CITESEQ2/
+#cp -R CITESEQ2/outs ~/GFS/PROJECTS/TfCf/Data/CITESEQ2/
+
