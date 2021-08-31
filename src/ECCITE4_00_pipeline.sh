5da2836f23375bcdcee8dbe9220803526507133c
Nikolaus Fortelny <nfortelny@came.sbg.ac.at>
2021-08-31 13:10:18 +0200

    x

--- src/ECCITE4_00_pipeline.sh
+++ src/ECCITE4_00_pipeline.sh
@@ -8,48 +8,81 @@ fi
 
 
 
-######### BASIC ANALYSIS
+# ######### BASIC ANALYSIS
+# cd $HOME/omicstmp
+#
+# for id in ECCITE4_Cas9 ECCITE4_WT; do
+#
+#     echo $id
+#
+#     echo "Files"
+#     cat $CODEBASE/tfcf/metadata/${id}_Library.csv
+#
+#     echo "Features"
+#     cat $CODEBASE/tfcf/metadata/ECCITE4_Features.csv
+#
+# 	~/code/cellranger-6.0.1/cellranger count --id=$id \
+# 	 --no-bam \
+# 	 --libraries=$CODEBASE/tfcf/metadata/${id}_Library.csv \
+# 	 --transcriptome=newGenomeExtended/ \
+# 	 --feature-ref=$CODEBASE/tfcf/metadata/ECCITE4_Features.csv \
+# 	 --localcores=24 \
+# 	 --localmem=128 \
+# 	 --expect-cells=10000 &> ${id}.log
+#
+# 	mkdir -p ~/GFS/PROJECTS/TfCf/Data/$id
+# 	mv $id/outs ~/GFS/PROJECTS/TfCf/Data/$id
+# done
+
+
+
+######### BASIC ANALYSIS without RNA
+
+
+# ######### AGGREGATE DATA
+# source $CODEBASE/tfcf/setup.sh
+# cd $HOME/omicstmp/
+#
+# id=ECCITE4_INT
+#
+# echo "sample_id,molecule_h5" > $id.csv
+# echo "Cas9,$DATA/ECCITE4_Cas9/outs/molecule_info.h5"  >> $id.csv
+# echo "WT,$DATA/ECCITE4_WT/outs/molecule_info.h5" >> $id.csv
+# cat $id.csv
+#
+# $HOME/code/cellranger-6.0.1/cellranger aggr --id=$id --csv=$id.csv --normalize=none &> $id.log
+#
+# mkdir -p ~/GFS/PROJECTS/TfCf/Data/$id/
+# mv $id/outs ~/GFS/PROJECTS/TfCf/Data/$id/
+
+
 
 
 cd $HOME/omicstmp
 
-for id in ECCITE4_Cas9 ECCITE4_WT; do
+for id_original in ECCITE4_Cas9 ECCITE4_WT; do
     
-    echo $id
-    
+    echo $id_original
+	id="${id_original}_onlyRNA"
+	echo $id
+
+	grep -v "CRISPR Guide Capture" $CODE/metadata/${id_original}_Library.csv > $CODE/metadata/${id}_Library.csv
+	
+	
     echo "Files"
     cat $CODEBASE/tfcf/metadata/${id}_Library.csv
-    
-    echo "Features"
-    cat $CODEBASE/tfcf/metadata/ECCITE4_Features.csv
     
 	~/code/cellranger-6.0.1/cellranger count --id=$id \
 	 --no-bam \
 	 --libraries=$CODEBASE/tfcf/metadata/${id}_Library.csv \
 	 --transcriptome=newGenomeExtended/ \
-	 --feature-ref=$CODEBASE/tfcf/metadata/ECCITE4_Features.csv \
-	 --localcores=24 \
-	 --localmem=128 \
+	 --localcores=8 \
+	 --localmem=24 \
 	 --expect-cells=10000 &> ${id}.log
     
-	mkdir -p ~/GFS/PROJECTS/TfCf/Data/$id
-	mv $id/outs ~/GFS/PROJECTS/TfCf/Data/$id
+	mkdir -p $DATA/$id
+	mv $id/outs $DATA/$id
 done
 
 
 
-######### AGGREGATE DATA
-source $CODEBASE/tfcf/setup.sh
-cd $HOME/omicstmp/
-
-id=ECCITE4_INT
-
-echo "sample_id,molecule_h5" > $id.csv
-echo "Cas9,$DATA/ECCITE4_Cas9/outs/molecule_info.h5"  >> $id.csv
-echo "WT,$DATA/ECCITE4_WT/outs/molecule_info.h5" >> $id.csv
-cat $id.csv
-
-$HOME/code/cellranger-6.0.1/cellranger aggr --id=$id --csv=$id.csv --normalize=none &> $id.log
-
-mkdir -p ~/GFS/PROJECTS/TfCf/Data/$id/
-mv $id/outs ~/GFS/PROJECTS/TfCf/Data/$id/
