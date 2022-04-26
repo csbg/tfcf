1e80128df46bf6b50fb669305386442d3db46d10
Nikolaus Fortelny <nfortelny@came.sbg.ac.at>
2022-04-26 13:03:22 +0200

    x

--- src/SCRNA_41_01_GESA.R
+++ src/SCRNA_41_01_GESA.R
@@ -23,13 +23,52 @@ DE.RES[abs(estimate) > 15, estimate := min(15,  abs(estimate)) * sign(estimate)]
 # fgsea -------------------------------------------------------------------
 gsea.res <- data.table()
 de.grp <- DE.RES$guide[1]
-for(de.grp in unique(DE.RES$guide)){
-  for(dbx in names(enr.terms)){
-    gsea.res <- rbind(gsea.res, data.table(fgsea(
-      pathways=enr.terms[[dbx]], 
-      stats=with(DE.RES[guide == de.grp], setNames(estimate, nm=gene_id))), 
-    grp=de.grp,
-    db=dbx))
+for(tx in unique(DE.RES$tissue)){
+  for(de.grp in unique(DE.RES[tissue == tx]$guide)){
+    for(dbx in names(enr.terms)){
+      gsea.res <- rbind(gsea.res, data.table(fgsea(
+        pathways=enr.terms[[dbx]], 
+        stats=with(DE.RES[tissue == tx][guide == de.grp], setNames(estimate, nm=gene_id))), 
+      grp=de.grp,
+      tissue=tx,
+      db=dbx))
+    }
   }
 }
 saveRDS(gsea.res, file=out("FGSEA.RDS"))
+
+
+
+# Plots -------------------------------------------------------------------
+if(!"gsea.res" %in% ls()) gsea.res <- readRDS(out("FGSEA.RDS"))
+
+# cleanup / export results
+gsea.res[is.nan(NES), NES := 0]
+gsea.res.export <- gsea.res[padj < 0.05][,-c("log2err", "NES", "size", "pval"),with=F]
+gsea.res.export$leadingEdge <- sapply(gsea.res.export$leadingEdge, function(vec) paste(vec[1:10], collapse = ","))
+write.tsv(gsea.res.export, out("GSEA_significant",".tsv"))
+
+for(dbx in unique(gsea.res$db)){
+  write.tsv(gsea.res.export[db == dbx], out("GSEA_significant_",dbx,".tsv"))
+}
+
+
+# Prepare for plotting
+for(dbx in unique(gsea.res$db)){
+  pDT <- gsea.res[db == dbx]
+  pw.display <- unique(pDT[padj < 0.05][order(NES)][, tail(.SD, n=5), by=c("grp", "tissue")]$pathway)
+  pDT <- pDT[pathway %in% pw.display]
+  pDT <- hierarch.ordering(pDT, "pathway", "grp", "NES", TRUE)
+  pDT <- hierarch.ordering(pDT, "grp", "pathway", "NES", TRUE)
+  ggplot(pDT, aes(x=grp, y=pathway, color=NES, size=pmin(5, -log10(padj)))) +
+    geom_point() + scale_color_gradient2(low="blue", mid="white", high="red") +
+    geom_point(data=pDT[padj < 0.05], shape=1, color="black") +
+    scale_size_continuous(range=c(0,5), limits = c(0,5)) +
+    theme_bw(12) +
+    xRot() +
+    facet_grid(. ~ tissue, space="free", scales="free") +
+    theme(strip.text.y=element_text(angle=0))
+  ggsave(out("GSEA_plot_",dbx,".pdf"), w=30,h=length(unique(pDT$pathway)) * 0.2 + 3, limitsize = FALSE)
+}
+
+
