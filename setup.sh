71ac56b3bfc649e9c6ce40a83ad234bbd6108262
Nikolaus Fortelny <nfortelny@came.sbg.ac.at>
2022-08-26 16:04:35 +0200

    x

---
+++ setup.sh
@@ -0,0 +1,18 @@
+export CODE="$HOME/code/tfcf/"
+export DATA="$GFS/PROJECTS/TfCf/Data/"
+export RAWDATA="$GFS/PROJECTS/TfCf/RawData/"
+export ANALYSIS="$GFS/PROJECTS/TfCf/Analysis/"
+
+
+# Get cellranger
+# wget --no-check-certificate -O cellranger-6.0.1.tar.gz 
+
+# Installe cite-seq count
+#conda create -n tfcf
+#conda install pip
+#pip install CITE-seq-Count==1.4.4
+
+
+# export PATH=/home/people/nfortelny/aws/files/v2/2.2.10/bin/:$PATH
+
+export PATH=$HOME/aws/aws-cli/v2/2.3.0/bin/:$PATH
