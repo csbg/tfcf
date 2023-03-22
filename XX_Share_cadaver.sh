b29b199da501079c5ff4a895a92a5af4a11b53c1
Nikolaus Fortelny <nfortelny@came.sbg.ac.at>
2023-03-22 17:02:57 +0100

    x

--- XX_Share_cadaver.sh
+++
@@ -1,8 +0,0 @@
-cd $GFS/PROJECTS/TfCf/Analysis/SHARE/
-cadfile="$HOME/tmp/cadfile.rcfile"
-touch $cadfile
-echo "mput *" >> $cadfile
-echo "exit" >> $cadfile
-cadaver -r $cadfile https://myfiles.sbg.ac.at/remote.php/dav/files/b1075184/papers/TFCF/DataDrop_NF/
-rm -rf *
-rm $cadfile
